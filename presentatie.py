ce5fc771a68cf8d5b817e4df5fead24e0e227f13
Vermote Davy <vermote_davy@hotmail.com>
2023-04-12 12:17:36 +0200

    boekhouding.py toegevoegd

---
+++ presentatie.py
@@ -0,0 +1,6 @@
+def presenteer(dictionary, totaal):
+    for k, v in dictionary.items():
+        print (k, ":", v, "euro")
+    print ("==========================")
+    print(f"totaal : {totaal} euro")
+    
